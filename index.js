a7d59fe5f7112c3ec00684c04a7afa74b3b798dc
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-04 19:53:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 module.exports = {name, height, message}
 
-function add(x,y){
-  return x+y
+function add(x, y){
+  return x + y
 }
-add (1+2)
+add (1, 2)
